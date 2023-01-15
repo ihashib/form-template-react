bbf7f56bc2ae2d3c87cda1ea413ebe66b9a1efe2
ihashib <ihashib2@gmail.com>
2023-01-15 17:09:50 +0600

    trying to implement use and set state

--- src/components/FormTemplate.js
+++ src/components/FormTemplate.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-export default class FormTemplate extends Component {
-  render() {
-    return (
-      <div className="main">
+export default function FormTemplate() {
+  let [user, setUser] = useState({email : '', password : ''});
+
+  const onSubmitHandler = (e) =>{
+    console.log(user);
+  }
+  const onChangeHandler = (e) => {
+    setUser({ ...user, [e.target.email]: e.target.password });
+  };
+
+
+  return (
+    <div className="main">
         <h1 className="title">Form Template</h1>
         <div className="form">
           <div className="form-main">
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form onSubmit={onSubmitHandler}>
+              <Form.Group className="mb-3" controlId="formBasicEmail" onChange={onChangeHandler}>
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" placeholder="Enter email" value={user.name} />
                 <Form.Text className="text-muted">
                   We'll never share your email with anyone else.
                 </Form.Text>
               </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Group className="mb-3" controlId="formBasicPassword" onChange={onChangeHandler}>
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" placeholder="Password" value={user.password} />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Check me out" />
@@ -32,6 +41,5 @@ export default class FormTemplate extends Component {
           </div>
         </div>
       </div >
-    )
-  }
+  )
 }
